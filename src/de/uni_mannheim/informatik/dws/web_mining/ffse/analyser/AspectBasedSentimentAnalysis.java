ab6158bbe15e39f456c57d79f2e607668ffdaea4
Arne Lange <arlange@mail.uni-mannheim.de>
2015-05-08 11:10:42 +0200

    updated word list for feature extraction

--- src/de/uni_mannheim/informatik/dws/web_mining/ffse/analyser/AspectBasedSentimentAnalysis.java
+++ src/de/uni_mannheim/informatik/dws/web_mining/ffse/analyser/AspectBasedSentimentAnalysis.java
@@ -1,6 +1,5 @@
 package de.uni_mannheim.informatik.dws.web_mining.ffse.analyser;
 
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -10,36 +9,31 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-
-
 public class AspectBasedSentimentAnalysis {
-	
+
 	private final static int maxNumberOfReviewsLoaded = 7000;
 	ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
 	public static Long activityTime = System.nanoTime();
 
-	
-	
 	public AspectBasedSentimentAnalysis() throws IOException {
 		int i = 0;
 		File[] files = new File("data/").listFiles();
-		for(File file:files) {
+		for (File file : files) {
 			Restaurant restaurant = new Restaurant();
 			restaurant.id = file.getName();
 			restaurant.load("data/");
-			if(restaurant.numOfReviews>0) {
+			if (restaurant.numOfReviews > 0) {
 				restaurants.add(restaurant);
 				i++;
 			}
-			if(i>=maxNumberOfReviewsLoaded) {
+			if (i >= maxNumberOfReviewsLoaded) {
 				break;
 			}
 		}
 	}
 
-	
 	/*
-	 *   MAIN 
+	 * MAIN
 	 */
 	public static void main(String[] args) throws Exception {
 		System.out.println("Start program");
@@ -49,7 +43,7 @@ public class AspectBasedSentimentAnalysis {
 		logActivity("Spellcorrected reviews");
 		absa.tagReviews();
 		logActivity("Tagged sentences of reviews");
-		//Review.clearNounfrequencies();
+		// Review.clearNounfrequencies();
 		Review.saveNounList();
 		logActivity("Saved nounlist");
 		HashSet<String> foods = absa.getFoodList();
@@ -62,46 +56,48 @@ public class AspectBasedSentimentAnalysis {
 		HashSet<String> priceValue = absa.getPriceValueList();
 		absa.replaceWordByCategoryInReviews("price_value", priceValue);
 		logActivity("replace price/value synonyms with price_value");
+		HashSet<String> atmosphere = absa.getAtmosphereList();
+		absa.replaceWordByCategoryInReviews("_atmosphere_", atmosphere);
+		logActivity("replace atmosphere synonyms with _atmosphere_");
 		absa.saveReviews();
 		System.out.println("Finished");
 
 	}
-	
+
 	public static void logActivity(String text) {
-		System.out.println(text + " in " + ((System.nanoTime()-activityTime)/1000000000.0f) + " seconds");
+		System.out.println(text + " in " + ((System.nanoTime() - activityTime) / 1000000000.0f) + " seconds");
 		activityTime = System.nanoTime();
 	}
-	
+
 	public void spellcorrectReviews() throws Exception {
 		WikiChecker.instance().loadDictionary();
 		try {
-			for(Restaurant restaurant:restaurants) {
+			for (Restaurant restaurant : restaurants) {
 				restaurant.spellcorrectReviews();
 			}
 		} finally {
 			WikiChecker.instance().saveDictionary();
 		}
 	}
-	
-	public void replaceWordByCategoryInReviews(String category, HashSet<String> wordlist){
-		for(Restaurant restaurant:restaurants) {
+
+	public void replaceWordByCategoryInReviews(String category, HashSet<String> wordlist) {
+		for (Restaurant restaurant : restaurants) {
 			restaurant.replaceWordByCategory(category, wordlist);
 		}
 	}
-	
+
 	public void saveReviews() throws IOException {
-		for(Restaurant restaurant:restaurants) {
+		for (Restaurant restaurant : restaurants) {
 			restaurant.save("data-preprocessed/");
 		}
 	}
-	
-	public void tagReviews()  {
-		for(Restaurant restaurant:restaurants) {
+
+	public void tagReviews() {
+		for (Restaurant restaurant : restaurants) {
 			restaurant.tagReviews();
 		}
 	}
-	
-	
+
 	private HashSet<String> getFoodList() {
 		HashSet<String> foodList = new HashSet<String>();
 		try {
@@ -120,9 +116,8 @@ public class AspectBasedSentimentAnalysis {
 		foodList.remove("at");
 		foodList.remove("as");
 		foodList.remove("am");
-		//custom words
+		// custom words
 		foodList.add("food");
-		
 		foodList.add("chicken");
 		foodList.add("corn");
 		foodList.add("nuggets");
@@ -174,7 +169,7 @@ public class AspectBasedSentimentAnalysis {
 		foodList.add("salads");
 		return foodList;
 	}
-	
+
 	private HashSet<String> getServiceList() {
 		HashSet<String> serviceList = new HashSet<String>();
 		serviceList.add("services");
@@ -187,9 +182,19 @@ public class AspectBasedSentimentAnalysis {
 		serviceList.add("barmaid");
 		serviceList.add("staff");
 		serviceList.add("management");
+		serviceList.add("serviceability");
+		serviceList.add("servicing");
+		serviceList.add("serving");
+		serviceList.add("labor");
+		serviceList.add("assist");
+		serviceList.add("assisting");
+		serviceList.add("work");
+		serviceList.add("working");
+		serviceList.add("appropriateness");
+		serviceList.add("duty");
 		return serviceList;
 	}
-	
+
 	// \d+([\,\.]\d{1,2})?\s*\$
 	private HashSet<String> getPriceValueList() {
 		HashSet<String> priceValueList = new HashSet<String>();
@@ -201,5 +206,35 @@ public class AspectBasedSentimentAnalysis {
 		priceValueList.add("price");
 		return priceValueList;
 	}
-}
 
+	private HashSet<String> getAtmosphereList() {
+		HashSet<String> atmosphereList = new HashSet<String>();
+		atmosphereList.add("ambience");
+		atmosphereList.add("ambiance");
+		atmosphereList.add("appearance");
+		atmosphereList.add("aura");
+		atmosphereList.add("feel");
+		atmosphereList.add("feeling");
+		atmosphereList.add("mood");
+		atmosphereList.add("presence");
+		atmosphereList.add("semblance");
+		atmosphereList.add("climate");
+		atmosphereList.add("surroundings");
+		atmosphereList.add("surrounding");
+		atmosphereList.add("culture");
+		atmosphereList.add("environment");
+		atmosphereList.add("locality");
+		atmosphereList.add("location");
+		atmosphereList.add("locale");
+		atmosphereList.add("scenery");
+		atmosphereList.add("place");
+		atmosphereList.add("view");
+		atmosphereList.add("outlook");
+		atmosphereList.add("setting");
+		atmosphereList.add("spot");
+		atmosphereList.add("decoration");
+		atmosphereList.add("decorated");
+		atmosphereList.add("setup");
+		return atmosphereList;
+	}
+}
